b6a92e03c0b90bd84f5af4d2822d26c5db7058c9
Oskar Rundgren <orundg@gmail.com>
2016-08-08 18:00:48 +0200

    circle class

---
+++ src/circle.cpp
@@ -0,0 +1,8 @@
+#include "circle.h"
+
+Circle::Circle()
+{
+  radius = 10.0;
+  positionX = 0.0;
+  positionY = 0.0;
+}
---
+++ src/circle.h
@@ -0,0 +1,13 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+class Circle
+{
+public:
+  Circle();
+  float radius;
+  float positionX;
+  float positionY;
+};
+
+#endif // CIRCLE_H
