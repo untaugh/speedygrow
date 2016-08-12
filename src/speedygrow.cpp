2575fd07917de19196b9dceb01889bd297f89387
Oskar Rundgren <orundg@gmail.com>
2016-08-12 08:33:30 +0200

    clean code

--- src/speedygrow.cpp
+++ src/speedygrow.cpp
@@ -3,59 +3,23 @@
 #include <GL/glew.h>
 #include "gl.hpp"
 
-/* A simple function that prints a message, the error code returned by SDL,
- * and quits the application */
-void sdldie(const char *msg)
-{
-    printf("%s: %s\n", msg, SDL_GetError());
-    SDL_Quit();
-    exit(1);
-}
-
-
-void checkSDLError(int line = -1)
-{
-#ifndef NDEBUG
-	const char *error = SDL_GetError();
-	if (*error != '\0')
-	{
-		printf("SDL Error: %s\n", error);
-		if (line != -1)
-			printf(" + line: %i\n", line);
-		SDL_ClearError();
-	}
-#endif
-}
-
 int main(int, char**){
 
 	SDL_Window *mainwindow; /* Our window handle */
 	SDL_GLContext maincontext; /* Our opengl context handle */
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) /* Initialize SDL's Video subsystem */
-			sdldie("Unable to initialize SDL"); /* Or die on error */
+	SDL_Init(SDL_INIT_VIDEO);
 
-	/* Request opengl 3.2 context.
-	 * SDL doesn't have the ability to choose which profile at this time of writing,
-	 * but it should default to the core profile */
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
-	/* Turn on double buffering with a 24bit Z buffer.
-	 * You may need to change this to 16 or 32 for your system */
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-	/* Create our window centered at 512x512 resolution */
-	mainwindow = SDL_CreateWindow("speedygrow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-			640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-	if (!mainwindow) /* Die if creation failed */
-			sdldie("Unable to create window");
-
-	checkSDLError(__LINE__);
+	mainwindow = SDL_CreateWindow("speedygrow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
 	maincontext = SDL_GL_CreateContext(mainwindow);
-	checkSDLError(__LINE__);
+
   SDL_GL_SetSwapInterval(1);
 
   Gl * gl = new Gl();
